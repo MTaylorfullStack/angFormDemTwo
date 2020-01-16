0aae4334ae74e0e30d6a548e5c05fb255bab6d30
MTaylorfullStack <mtaylor@codingdojo.com>
2020-01-16 15:13:15 -0600

    Committed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Pet } from './models/pet';
+import { format } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'disAng';
+  pets: Pet[] = [];
+  pet = new Pet();
+  title = 'Hello World';
+  name = 'allUsersFromDB';
+  processPet(form: ngForm) {
+    console.log('You have submitted the form!');
+    console.log(this.pet);
+    this.pets.push(this.pet);
+    this.pet = new Pet();
+    form.reset();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +8,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
